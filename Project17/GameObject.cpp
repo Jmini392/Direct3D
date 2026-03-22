20e9491405e60d4a0896cef65396f4d1478ab9f5
Jmini392 <131459220+Jmini392@users.noreply.github.com>
2026-03-22 22:33:45 +0900

    프로젝트17 미완 빌드오류

---
+++ Project17/GameObject.cpp
@@ -0,0 +1,160 @@
+#include "GameObject.h"
+#include "Shader.h"
+
+CGameObject::CGameObject() {
+	XMStoreFloat4x4(&m_xmf4x4World, XMMatrixIdentity());
+}
+
+CGameObject::~CGameObject() {
+	if (m_pMesh) m_pMesh->Release();
+	if (m_pMaterial) m_pMaterial->Release();
+}
+
+void CGameObject::SetShader(CShader* pShader) {
+	if (!m_pMaterial) {
+		m_pMaterial = new CMaterial();
+		m_pMaterial->AddRef();
+	}
+	if (m_pMaterial) m_pMaterial->SetShader(pShader);
+}
+
+void CGameObject::SetMesh(CMesh* pMesh) {
+	if (m_pMesh) m_pMesh->Release();
+	m_pMesh = pMesh;
+	if (m_pMesh) m_pMesh->AddRef();
+}
+
+void CGameObject::ReleaseUploadBuffers() {
+	//정점 버퍼를 위한 업로드 버퍼를 소멸시킨다.
+	if (m_pMesh) m_pMesh->ReleaseUploadBuffers();
+}
+
+void CGameObject::Animate(float fTimeElapsed) { }
+
+void CGameObject::OnPrepareRender() { }
+
+void CGameObject::Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera) {
+	OnPrepareRender();
+
+	if (m_pMaterial) {
+		if (m_pMaterial->m_pShader) {
+			m_pMaterial->m_pShader->Render(pd3dCommandList, pCamera);
+			m_pMaterial->m_pShader->UpdateShaderVariable(pd3dCommandList, &m_xmf4x4World);
+		}
+	}
+	if (m_pMesh) m_pMesh->Render(pd3dCommandList);
+}
+
+void CGameObject::Rotate(XMFLOAT3* pxmf3Axis, float fAngle) {
+	XMMATRIX mtxRotate = XMMatrixRotationAxis(XMLoadFloat3(pxmf3Axis), XMConvertToRadians(fAngle));
+	m_xmf4x4World = Matrix4x4::Multiply(mtxRotate, m_xmf4x4World);
+}
+
+void CGameObject::CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList) { }
+
+void CGameObject::ReleaseShaderVariables() { }
+
+void CGameObject::UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList) {
+	XMFLOAT4X4 xmf4x4World;
+	XMStoreFloat4x4(&xmf4x4World, XMMatrixTranspose(XMLoadFloat4x4(&m_xmf4x4World)));
+	//객체의 월드 변환 행렬을 루트 상수(32-비트 값)를 통하여 셰이더 변수(상수 버퍼)로 복사한다.
+	pd3dCommandList->SetGraphicsRoot32BitConstants(0, 16, &xmf4x4World, 0);
+}
+
+void CGameObject::SetPosition(float x, float y, float z) {
+	m_xmf4x4World._41 = x;
+	m_xmf4x4World._42 = y;
+	m_xmf4x4World._43 = z;
+}
+
+void CGameObject::SetPosition(XMFLOAT3 xmf3Position) {
+	SetPosition(xmf3Position.x, xmf3Position.y, xmf3Position.z);
+}
+
+XMFLOAT3 CGameObject::GetPosition() {
+	return(XMFLOAT3(m_xmf4x4World._41, m_xmf4x4World._42, m_xmf4x4World._43));
+}
+
+//게임 객체의 로컬 z-축 벡터를 반환한다.
+XMFLOAT3 CGameObject::GetLook() {
+	return(Vector3::Normalize(XMFLOAT3(m_xmf4x4World._31, m_xmf4x4World._32, m_xmf4x4World._33)));
+}
+
+//게임 객체의 로컬 y-축 벡터를 반환한다.
+XMFLOAT3 CGameObject::GetUp() {
+	return(Vector3::Normalize(XMFLOAT3(m_xmf4x4World._21, m_xmf4x4World._22, m_xmf4x4World._23)));
+}
+
+//게임 객체의 로컬 x-축 벡터를 반환한다.
+XMFLOAT3 CGameObject::GetRight() {
+	return(Vector3::Normalize(XMFLOAT3(m_xmf4x4World._11, m_xmf4x4World._12, m_xmf4x4World._13)));
+}
+
+//게임 객체를 로컬 x-축 방향으로 이동한다.
+void CGameObject::MoveStrafe(float fDistance) {
+	XMFLOAT3 xmf3Position = GetPosition();
+	XMFLOAT3 xmf3Right = GetRight();
+	xmf3Position = Vector3::Add(xmf3Position, xmf3Right, fDistance);
+	CGameObject::SetPosition(xmf3Position);
+}
+
+//게임 객체를 로컬 y-축 방향으로 이동한다.
+void CGameObject::MoveUp(float fDistance) {
+	XMFLOAT3 xmf3Position = GetPosition();
+	XMFLOAT3 xmf3Up = GetUp();
+	xmf3Position = Vector3::Add(xmf3Position, xmf3Up, fDistance);
+	CGameObject::SetPosition(xmf3Position);
+}
+
+//게임 객체를 로컬 z-축 방향으로 이동한다.
+void CGameObject::MoveForward(float fDistance) {
+	XMFLOAT3 xmf3Position = GetPosition();
+	XMFLOAT3 xmf3Look = GetLook();
+	xmf3Position = Vector3::Add(xmf3Position, xmf3Look, fDistance);
+	CGameObject::SetPosition(xmf3Position);
+}
+
+//게임 객체를 주어진 각도로 회전한다.
+void CGameObject::Rotate(float fPitch, float fYaw, float fRoll) {
+	XMMATRIX mtxRotate = XMMatrixRotationRollPitchYaw(XMConvertToRadians(fPitch), XMConvertToRadians(fYaw), XMConvertToRadians(fRoll));
+	m_xmf4x4World = Matrix4x4::Multiply(mtxRotate, m_xmf4x4World);
+}
+
+void CGameObject::SetMaterial(CMaterial* pMaterial) {
+	if (m_pMaterial) m_pMaterial->Release();
+	m_pMaterial = pMaterial;
+	if (m_pMaterial) m_pMaterial->AddRef();
+}
+
+void CGameObject::SetMaterial(UINT nReflection) {
+	if (!m_pMaterial) m_pMaterial = new CMaterial();
+	m_pMaterial->m_nReflection = nReflection;
+}
+
+CRotatingObject::CRotatingObject() {
+	m_xmf3RotationAxis = XMFLOAT3(0.0f, 1.0f, 0.0f);
+	m_fRotationSpeed = 90.0f;
+}
+
+CRotatingObject::~CRotatingObject() { }
+
+void CRotatingObject::Animate(float fTimeElapsed) {
+	CGameObject::Rotate(&m_xmf3RotationAxis, m_fRotationSpeed * fTimeElapsed);
+}
+
+CMaterial::CMaterial() {
+	m_xmf4Albedo = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+CMaterial::~CMaterial() {
+	if (m_pShader) {
+		m_pShader->ReleaseShaderVariables();
+		m_pShader->Release();
+	}
+}
+
+void CMaterial::SetShader(CShader* pShader) {
+	if (m_pShader) m_pShader->Release();
+	m_pShader = pShader;
+	if (m_pShader) m_pShader->AddRef();
+}
